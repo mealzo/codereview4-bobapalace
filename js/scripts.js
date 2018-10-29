58198c93df9878332317036c3271ff0f67c8fcce
Amelia Ralston-Okabayashi <a.ralston.o@gmail.com>
2018-10-28 17:38:37 -0700

    finished my project

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+//global variables
 
 //boba constructor
-function Boba() {
-  this.size=size;
-  this.flavors=[];
-  this.milk=milk;
-  this.fillings=[];
+function Boba(size, flavors, milks, fillings, total) {
+  this.size = size;
+  this.flavors = flavors;
+  this.milks = milks;
+  this.fillings = fillings;
+  this.total = total;
 }
+
 //boba price function
 Boba.prototype.price = function(){
   var total = 0;
@@ -19,6 +22,9 @@ Boba.prototype.price = function(){
   if (this.milk==="soy") {
       total+=.5;
   }
+  if(this.fillings.length > 1){
+    total+=this.fillings.length
+  }
   return total;
 }
 
@@ -28,11 +34,38 @@ Boba.prototype.price = function(){
 $("document").ready(function(){
   $(".boba-form").submit(function(event){
     event.preventDefault();
-    var order = new Boba;
-    $(".order").show();
-    $("input:checkbox[name=boba-flavor]:checked").each(function(){
-      var bobaFlavor = $(this).val();
-      console.log(bobaFlavor)
+    var name = prompt("Who will be receiving boba?")
+    //get the values of the checkboxes and store them in a var
+    var bobaSize = "";
+     $("input:checkbox[name=boba-size]:checked").each(function(){
+      bobaSize = $(this).val();
     });
+    var bobaFlavors = [];
+    $("input:checkbox[name=boba-flavor]:checked").each(function(){
+      bobaFlavors.push($(this).val());
+
+    })
+    var bobaMilks = [];
+    $("input:checkbox[name=boba-milk]:checked").each(function(){
+      bobaMilks.push($(this).val());
+    });
+    var bobaFillings = [];
+    $("input:checkbox[name=boba-fillings]:checked").each(function(){
+      bobaFillings.push($(this).val());
+    });
+    //make the new object
+    var boba1 = new Boba(bobaSize, bobaFlavors, bobaMilks, bobaFillings);
+    //get the price of the object
+    var y = boba1.price();
+    //make arrays correct grammatically
+    var flavors = boba1.flavors.join(" and ")
+    var milks = boba1.milks.join(" and ")
+    var fillings = boba1.fillings.join(", ")
+    //write the text
+    $(".order").append("Your " + boba1.size + ", " + flavors + " boba with " + milks + " milk, and " + fillings + " fillings. will be on the way to " + name +   " in 20mins or less. Your total is $" + y);
+    $(".order").show();
+    console.log(boba1);
+
+
   })
 });
